e1e3eed5ad90bb0255c4010673888ce6c2dde701
GustavoLSantos <gustavolirasn@gmail.com>
2024-04-17 21:03:41 -0300

    Render Text Statistics

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type ValueProps = {
+  label: string,
+  value: string,
+}
+
+const Value = ({label, value}: ValueProps) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+)
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ flexDirection: 'row'}}>
+        <Value label="Steps" value={'1219'}/>
+        <Value label="Distance" value={'0,75 km'}/>
+      </View>
+      <Value label="Flights Climbed" value={'6'}/>      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    padding: 12,
+  },
+  valueContainer: {
+    marginRight: 50,
+    marginVertical: 10,
+  },
+  label: {
+    fontSize: 20,
+    color: 'white',
+  },
+  value: {
+    fontSize: 35,
+    color: '#AFB3BE',
+    fontWeight: '500',
   },
 });
